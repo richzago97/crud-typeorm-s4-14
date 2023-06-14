bb7ab1ed7f5546f34a0ca3f668365b146378383b
richzago97 <rich.czago@gmail.com>
2023-06-14 14:40:18 -0300

    feat: adicionado o remove password com o metodo map

--- src/services/user/userList.service.ts
+++ src/services/user/userList.service.ts
@@ -1,11 +1,14 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
+import { removePassword } from "../../utils/removePassword";
 
 const userListService = async (): Promise<User[]> => {
-  const userRepository = AppDataSource.getRepository(User);
+   const userRepository = AppDataSource.getRepository(User);
 
-  const users = userRepository.find();
+   const users = await userRepository.find();
 
-  return users;
+   const usersWithoutPassword = users.map((user: User) => removePassword(user));
+
+   return usersWithoutPassword;
 };
 export default userListService;
